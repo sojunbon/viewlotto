c502040b7e835bf9d35818cc0a43d3d4605ea212
sojunbon <supasit.ch@hotmail.com>
2026-03-26 16:07:09 +0700

    price input v4

--- lib/price_input_screen.dart
+++ lib/price_input_screen.dart
@@ -3,6 +3,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
+// หน้าจอสำหรับระบุราคาที่ต้องการแทง (หลังจากเลือกเลขและประเภทในหน้า Draft) โดยมีการคำนวณอัตราจ่ายแบบขั้นบันได และแสดงยอดเงินคงเหลือแบบ Real-time
 const Color kMainGreen = Color(0xFF11998E);
 
 class PriceInputScreen extends StatefulWidget {
